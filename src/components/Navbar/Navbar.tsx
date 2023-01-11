7c00aa81906a8ad25a189ccb0e67c2e8e364408b
savagem7 <i0g1n4oj>
2023-01-11 22:36:41 +0000

    add navbar and spotify component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { Card } from "@mantine/core";
 import styles from "./navbar.module.scss";
+
+interface NavItemProps {
+  href: string;
+  name: string;
+}
+
+const NavItem = ({ href, name }: NavItemProps) => {
+  const router = useLocation();
+  return (
+    <Link href={href} className={router.pathname === href ? styles.navActive : styles.navItem}>
+      {name}
+    </Link>
+  );
+};
 
 export const Navbar = () => {
   return (
     <div className={styles.container}>
       <nav>
         <div className={styles.navItems}>
-          <Link to="/#about-me">Home</Link>
-          <Link to="/#interests">Interests</Link>
-          <Link to="/#projects">Projects</Link>
-          <Link to="/cv">CV</Link>
+          <NavItem href="/" name="Home" />
+          <NavItem href="/interests" name="Interests" />
+          <NavItem href="/projects" name="Projects" />
+          <NavItem href="/cv" name="CV" />
         </div>
+        <Card shadow="sm" p="lg" radius="md" withBorder className={styles.spotifyListening}>
+          What am I listening too?
+        </Card>
       </nav>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   build: { outDir: "build" },
-  server: { port: 3000 },
+  server: { port: 3001 },
 });
