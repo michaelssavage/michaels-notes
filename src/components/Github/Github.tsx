bf6ae652f91db089ae6c08a5d8d22014541a0eec
michael <msavage3@jaguarlandrover.com>
2023-01-17 16:10:44 +0000

    add animXYZ package

--- src/components/Github/Github.tsx
+++ src/components/Github/Github.tsx
@@ -1,5 +1,6 @@
 import useSWR from "swr";
 import { Image, SimpleGrid, Text } from "@mantine/core";
+import { XyzTransitionGroup } from "@animxyz/react";
 import { getGithubContent } from "api/graphic-design";
 import { GITHUB_CONTENT } from "utils/constants";
 import { GraphicsProps } from "utils/github-types";
@@ -13,18 +14,23 @@ export const Github = () => {
 
       <Text>Artwork created using Adobe Illustrator</Text>
 
-      <SimpleGrid cols={6} style={{ margin: "1rem 0" }}>
-        {data &&
-          data.map((graphic) => (
-            <div
-              key={graphic.key}
-              style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-            >
-              <Image radius="md" src={graphic.img} width={200} alt={`${graphic.name} image`} />
-              <Text> {graphic.name} </Text>
-            </div>
-          ))}
-      </SimpleGrid>
+      <XyzTransitionGroup
+        appearVisible
+        xyz="fade up in-left in-rotate-left out-right out-rotate-right"
+      >
+        <SimpleGrid cols={6} style={{ margin: "1rem 0" }}>
+          {data &&
+            data.map((graphic) => (
+              <div
+                key={graphic.key}
+                style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+              >
+                <Image radius="md" src={graphic.img} width={200} alt={`${graphic.name} image`} />
+                <Text> {graphic.name} </Text>
+              </div>
+            ))}
+        </SimpleGrid>
+      </XyzTransitionGroup>
     </section>
   );
 };
--- src/components/Spotify/TopTracks/TopTracks.tsx
+++ src/components/Spotify/TopTracks/TopTracks.tsx
@@ -1,5 +1,6 @@
 import useSWR from "swr";
 import { Image, SimpleGrid, Text } from "@mantine/core";
+import { XyzTransitionGroup } from "@animxyz/react";
 import { TOP_TRACKS_ENDPOINT } from "utils/constants";
 import { getTopTracks } from "api/spotify";
 import { TopTracksProps } from "utils/spotify-types";
@@ -13,25 +14,30 @@ export const TopTracks = () => {
 
       <Text>My Favourite Tracks pulled from Spotify</Text>
 
-      <SimpleGrid cols={6} style={{ margin: "1rem 0" }}>
-        {data &&
-          data.map((track: TopTracksProps) => (
-            <div
-              key={track.songUrl}
-              style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-            >
-              <Image
-                radius="md"
-                src={track.albumImageUrl}
-                alt={`${track.title} album art`}
-                width={200}
-              />
-              <Text style={{ marginTop: "1rem" }}>
-                {track.artist} - {track.title}
-              </Text>
-            </div>
-          ))}
-      </SimpleGrid>
+      <XyzTransitionGroup
+        appearVisible
+        xyz="fade up in-left in-rotate-left out-right out-rotate-right"
+      >
+        <SimpleGrid cols={6} style={{ margin: "1rem 0" }}>
+          {data &&
+            data.map((track: TopTracksProps) => (
+              <div
+                key={track.songUrl}
+                style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+              >
+                <Image
+                  radius="md"
+                  src={track.albumImageUrl}
+                  alt={`${track.title} album art`}
+                  width={200}
+                />
+                <Text style={{ marginTop: "1rem" }}>
+                  {track.artist} - {track.title}
+                </Text>
+              </div>
+            ))}
+        </SimpleGrid>
+      </XyzTransitionGroup>
     </section>
   );
 };
