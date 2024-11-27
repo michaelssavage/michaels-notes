edf3131443da612538836c551c166790f0fed9f0
Michael S <33690140+michaelssavage@users.noreply.github.com>
2024-11-27 11:25:47 +0100

    Update pretty-text.tsx

--- src/routes/blog/pretty-text.tsx
+++ src/routes/blog/pretty-text.tsx
@@ -56,7 +56,6 @@ function RekordboxPrettifier() {
 				});
 			} else {
 				// If the expected headers are not found, return the original text
-				toast.warn("The expected headers weren't found, returning the original text");
 				return text;
 			}
 		}
