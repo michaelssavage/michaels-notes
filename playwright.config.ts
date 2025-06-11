8dadc34c0b2095ff71c40291d030111779431f1f
micha <michaelsavage940@gmail.com>
2025-06-11 23:54:05 +0200

    web server changes for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,9 +70,9 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
+  /* Run the local dev server before starting the tests */
+  webServer: process.env.CI ? undefined : {
+    command: 'netlify dev',
     url: 'http://localhost:8888',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000, // 2 minutes
