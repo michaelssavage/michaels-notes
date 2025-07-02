25644bed464838f9f8919f9554e9ae725ca89a74
Michael <michael@talentbait.com>
2025-07-02 15:55:42 +0200

    add react testing post

--- src/components/content/Code.tsx
+++ src/components/content/Code.tsx
@@ -1,10 +1,12 @@
 import styled from "@emotion/styled";
+import { darken } from "polished";
 import type { HTMLAttributes } from "react";
 
 const Style = styled.code`
   &:not(pre > code) {
     font-family: monospace;
     color: ${({ theme }) => theme.colors.code};
+    background-color: ${({ theme }) => darken(0.06, theme.colors.background)};
     font-size: 1.2rem;
     font-weight: 700;
   }
