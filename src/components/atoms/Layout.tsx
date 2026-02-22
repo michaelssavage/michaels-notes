c17db076ec87a8a837c6fa2a68bb1cf3ca7d752a
Michael Savage <michaelsavage940@gmail.com>
2026-02-22 21:21:34 +0100

    reset the layout size

--- src/components/atoms/Layout.tsx
+++ src/components/atoms/Layout.tsx
@@ -1,13 +1,7 @@
-import { forTabletOnly } from "@/styles/abstracts/mixins.styled";
-import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
 const Container = styled.div`
-  padding-top: 110px;
-
-  ${forTabletOnly(css`
-    padding-top: 80px;
-  `)}
+  padding-top: var(--header-height, 0px);
 `;
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
