dd83e2d6b71e4c923f956da1476fbf963325804c
Michael Savage <michaelsavage940@gmail.com>
2026-07-01 00:03:48 +0200

    always show the feedback textarea in the footer

--- src/components/molecules/Feedback/Feedback.tsx
+++ src/components/molecules/Feedback/Feedback.tsx
@@ -2,26 +2,21 @@ import { submitFeedback } from "@/api/d1/feedback.api";
 import { Group } from "@/components/atoms/Group";
 import { TextArea } from "@/components/form/TextArea";
 import { CheckIcon } from "@/components/icons";
-import { MessageHeartIcon } from "@/components/icons/MessageHeart";
 import { SendIcon } from "@/components/icons/Send";
 import { Button } from "@/components/molecules/Button";
-import { css } from "@emotion/react";
 import { lazy, useState } from "react";
 import {
-  ButtonGrid,
   Card,
   ErrorMessage,
   feedbackBtnStyles,
-  HeartContainer,
   SuccessMessage,
 } from "./Feedback.styled";
 
 const LoaderIcon = lazy(() =>
-  import("react-hot-toast").then((mod) => ({ default: mod.LoaderIcon }))
+  import("react-hot-toast").then((mod) => ({ default: mod.LoaderIcon })),
 );
 
 export const Feedback = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
   const [feedback, setFeedback] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
@@ -42,8 +37,7 @@ export const Feedback = () => {
 
         setTimeout(() => {
           setIsSubmitted(false);
-          setIsExpanded(false);
-        }, 2000);
+        }, 5000);
       }
     } catch (err: unknown) {
       if (err instanceof Response && err.status === 429) {
@@ -57,59 +51,34 @@ export const Feedback = () => {
   };
 
   return (
-    <>
-      {isExpanded ? (
-        <Card>
-          {isSubmitted ? (
-            <SuccessMessage>
-              <Group direction="row" align="center" gap="0.5rem">
-                <CheckIcon size={24} />
-                <p>Thanks for your feedback!</p>
-              </Group>
-            </SuccessMessage>
-          ) : (
-            <>
-              <TextArea
-                id="feedback"
-                name="feedback"
-                placeholder="Leave feedback or say hello!"
-                value={feedback}
-                onChange={(e) => setFeedback(e.target.value)}
-                maxLen={2000}
-              />
-              {error && <ErrorMessage>{error}</ErrorMessage>}
-              <ButtonGrid>
-                <Button
-                  text="Close"
-                  onClick={() => setIsExpanded(false)}
-                  variant="secondary"
-                  styles={css`
-                    flex: 1;
-                    justify-content: center;
-                  `}
-                />
-                <Button
-                  onClick={handleSubmit}
-                  disabled={!feedback.trim() || isSubmitting}
-                  styles={feedbackBtnStyles}
-                  text={isSubmitting ? "Sending..." : "Send"}
-                  icon={isSubmitting ? <LoaderIcon /> : <SendIcon size={16} />}
-                />
-              </ButtonGrid>
-            </>
-          )}
-        </Card>
-      ) : null}
-      <Button
-        onClick={() => setIsExpanded(true)}
-        icon={
-          <HeartContainer>
-            <MessageHeartIcon size={24} />
-          </HeartContainer>
-        }
-        text="Feedback"
-        variant="ghost"
-      />
-    </>
+    <Card>
+      {isSubmitted ? (
+        <SuccessMessage>
+          <Group direction="row" align="center" gap="0.5rem">
+            <CheckIcon size={24} />
+            <p>Thank you!</p>
+          </Group>
+        </SuccessMessage>
+      ) : (
+        <>
+          <TextArea
+            id="feedback"
+            name="feedback"
+            placeholder="Leave feedback or say hello!"
+            value={feedback}
+            onChange={(e) => setFeedback(e.target.value)}
+            maxLen={2000}
+          />
+          {error && <ErrorMessage>{error}</ErrorMessage>}
+          <Button
+            onClick={handleSubmit}
+            disabled={!feedback.trim() || isSubmitting}
+            styles={feedbackBtnStyles}
+            text={isSubmitting ? "Sending..." : "Send"}
+            icon={isSubmitting ? <LoaderIcon /> : <SendIcon size={16} />}
+          />
+        </>
+      )}
+    </Card>
   );
 };
--- src/components/molecules/Footer/Footer.styled.ts
+++ src/components/molecules/Footer/Footer.styled.ts
@@ -2,16 +2,24 @@ import { forPhoneOnly } from "@/styles/abstracts/mixins.styled";
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
+export const FooterWrapper = styled.footer`
+  padding: 0 1rem;
+  margin: 2rem 0 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+`;
+
 export const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 1000px;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
   flex-wrap: wrap;
-
-  padding: 0 1rem;
-  max-width: 1000px;
-  margin: 3rem auto;
   gap: 1rem;
 
   ${forPhoneOnly(css`
